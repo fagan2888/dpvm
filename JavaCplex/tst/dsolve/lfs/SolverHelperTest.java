4bb3ab0bf872712a044d48f46c8e9d8adc83dab9
Mihai Cimpoesu <mihai@imcu.ro>
2012-12-09 15:25:15 +0200

    succesfully loading native cplex library from jar

--- JavaCplex/tst/dsolve/lfs/SolverHelperTest.java
+++ JavaCplex/tst/dsolve/lfs/SolverHelperTest.java
@@ -1,11 +1,12 @@
 package dsolve.lfs;
 
-import com.sun.xml.internal.ws.policy.AssertionSet;
 import dsolve.SolverHelper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,8 +17,26 @@ import java.io.IOException;
 
 public class SolverHelperTest {
 	@Test
-	public void testDroppingNativeLibrary() throws IOException {
-		SolverHelper.dropNativeCplex( "cplex124.dll" );
-		Assert.assertTrue( System.getProperty( "java.library.path" ) != null );
+	public void testDroppingNativeLibrary() throws IOException, URISyntaxException {
+		SolverHelper.dropNativeCplex();
+		String []path = System.getProperty( "java.library.path" ).split( ";" );
+		String dropPath = path[path.length-1];
+		System.out.println( "droppath : " + dropPath );
+
+		File[] dropPathFiles = new File( dropPath ).listFiles();
+		boolean foundDll = false;
+		for ( File dll : dropPathFiles ) {
+			if( dll.getAbsolutePath().contains( "cplex124" ) ) {
+				foundDll = true;
+				System.out.println( "found dll: " + dll.getAbsolutePath() );
+				break;
+			}
+		}
+		Assert.assertTrue( "cplex dll could not be found in classpath", foundDll );
+	}
+
+	public static void main( String[] args ) throws IOException, URISyntaxException {
+		SolverHelper.listJarFiles( "cplex124.dll" );
+		new SolverHelperTest().testDroppingNativeLibrary();
 	}
 }
